fd124449cf5df65cac8e7d317c5d91df2d053755
akiestadanni <akiestadanni@gmail.com>
2023-10-12 13:43:33 -0500

    cree archivos index,PasEvent y upcomingEvents.js

---
+++ JSindex.js
@@ -0,0 +1,41 @@
+// Aquí encontrarás los pasos para completar tu segundo sprint.
+// Tenga en cuenta los comentarios que recibió en su sprint anterior y realice los cambios si es necesario.
+
+// Para este Sprint necesitarás el contenido proporcionado en estos talleres:
+// _Introducción a JavaScript
+// _Funciones
+// _Condicionales
+// _Arrays y objetos
+// _Loops
+// _DOM
+
+// ❗Recuerda que trabajarás en una nueva rama para cada sprint. Este debería ser "sprint2"
+
+
+// 2do sprint
+
+// ◾Crearás 3 archivos JS:
+// -para la página de inicio
+// -para la página de Eventos pasados
+// ​​-para la página de Próximos eventos
+
+// ◾Vincula tus archivos JS a su html correspondiente ⁓puedes verificar si se hizo correctamente con un console.log(“ Hola")⁓
+
+// ◾Cambie la extensión del archivo de datos a .js
+
+// ◾Vincula tu archivo de datos a tus archivos HTML. (Recuerda que tiene que estar encima del archivo JS que utilizarás).
+
+// ◾Tendrás que filtrar los eventos en los datos por fecha, usando js, ​​según en qué página te encuentres:
+// -Home mostrará todos los eventos.
+// -Pasado mostrará los eventos realizados antes de la “fecha actual” en los datos.
+// -Próximo mostrará los eventos que se realizarán después de la “fecha actual” en los datos.
+
+// ◾Tendrás que mostrar la información de los eventos en cada página tal como lo hiciste para el 1er sprint usando JavaScript.
+
+// ◾ Intente ordenar su código en funciones de responsabilidad únicas.
+
+// ◾Esté atento a espacios y comentarios innecesarios en el código.
+
+// ◾ Una vez que haya terminado con su trabajo, agregue, confirme y envíe su proyecto a la rama (sprint2).
+
+// ◾Envía tu sprint dejando el enlace a tu repositorio de GitHub.
