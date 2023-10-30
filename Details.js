d099d7ca4a5785d05bfd3db29c4d48a16a6f6e91
akiestadanni <akiestadanni@gmail.com>
2023-10-30 17:33:45 -0500

    ajuste funcion en search

--- Details.js
+++ Details.js
@@ -1,65 +1,65 @@
 // pintar card ditails
 const urlParams= new URLSearchParams(window.location.search)
-console.log(urlParams);
 const idEvento=urlParams.get("_id")
+let eventoBuscado= todosLosEventos.find(evento=>evento._id==idEvento)
+console.log(urlParams);
 console.log(idEvento);
-const eventos= data.events
-let eventoBuscado= eventos.find(evento=>evento._id==idEvento)
-console.table(eventoBuscado);
+console.log(eventoBuscado);
 
 // esta funcion pinta la card de details
-function crearCard(idEvento) {
+function crearCard(ArrayDeEvento) {
   const contenedorCardDetails= document.getElementById("card-details")
   const estructuraCardDetails= document.createElement("div")
-  estructuraCardDetails.classList.add("row","g-0")
+  estructuraCardDetails.classList.add("row","contain-fluid")
   estructuraCardDetails.innerHTML= `<div class="col-md-6 d-flex justify-content-between border border-light">
-  <img src="${idEvento.image} " class="img-fluid rounded-start object-fit-cover"
+  <img src=${ArrayDeEvento.image}class="img-fluid rounded-start object-fit-cover"
       alt="music_concert.jpg">
   </div>
   <div class="col-md-6 justify-content-center bg-danger-subtle p-2">
   <div class="card-body">
-  <h4 class="card-title p-1 m-3 d-flex justify-content-evenly">${idEvento.name}</h4>
+  <h4 class="card-title p-1 m-3 d-flex justify-content-evenly">${ArrayDeEvento.name}</h4>
   
   <h5>Date:</h5>
   <p class="card-text"><small class="text-body-secondary"></small>
-  ${idEvento.date}
+  ${ArrayDeEvento.date}
   </p>
   
   <h5 class="description">Description:</h5>
   <p class="card-text">
-  ${idEvento.description}
+  ${ArrayDeEvento.description}
   </p>
   
   <h5>Category:</h5>
   <p class="card-text"><small class="text-body-secondary">
-  ${idEvento.category}
+  ${ArrayDeEvento.category}
   </small></p>
   
   <h5>Place:</h5>
   <p class="card-text"><small class="text-body-secondary">
-  ${idEvento.place}
+  ${ArrayDeEvento.place}
   </small></p>
   
   <h5>Capacity :</h5>
   <p class="card-text"><small class="text-body-secondary">
-  ${idEvento.capacity}
+  ${ArrayDeEvento.capacity}
   </small></p>
   
   <h5>Estimate:</h5>
   <p class="card-text"><small class="text-body-secondary">
-  ${idEvento.estimate}
+  ${ArrayDeEvento.estimate}
   </small></p>
   
   <h5>assistance:</h5>
   <p class="card-text"><small class="text-body-secondary">
-  ${idEvento.assistance}
+  ${ArrayDeEvento.assistance}
   </small></p>
   
   <h5>Price:</h5>
   <p class="card-text"><small class="text-body-secondary">
-  ${idEvento.price}></p>
+  ${ArrayDeEvento.price}</p>
   </div>
   </div>`;
   contenedorCardDetails.appendChild(estructuraCardDetails)
+  console.log(estructuraCardDetails);
   };
 crearCard(eventoBuscado);
