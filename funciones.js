4d5d4050b5b62f734e58bdfcce60f4b067dda19e
akiestadanni <akiestadanni@gmail.com>
2023-10-31 12:52:43 -0500

    cambios revisados(:

--- funciones.js
+++ funciones.js
@@ -1,3 +1,26 @@
+export const contenedorSearch = document.getElementById("search");
+
+export function filtrarEventosChecked(eventos) {
+    checboxActivados = Array.from(document.querySelectorAll('input[type="Checkbox"]:checked')).map(checkbox => checkbox.value)
+    if (checboxActivados.length > 0) {
+        return todosLosEventos.filter(categoria => checboxActivados.includes(categoria.category))
+    } return eventos
+}
+
+export function filtrarTextoIngresado(eventos) {
+   console.log(contenedorSearch.value);
+    return eventos.filter(evento=> evento.name.toUpperCase().includes(contenedorSearch.value.toUpperCase()))
+}
+contenedorCheckbox.addEventListener('change',()=>{
+
+
+});
+
+contenedorSearch.addEventListener('keyup',()=>{
+    let filtroEventosChecked= filtrarEventosChecked(todosLosEventos)
+    let filtroCoincidencias=filtrarTextoIngresado(filtroEventosChecked)
+   crearCards(filtroCoincidencias)
+});
 // extrayendo datos de la data
 const todosLosEventos = data.events;
 const fechaActual = data.currentDate;
@@ -64,3 +87,27 @@ const newTodosLosEventos= data.events
 const newFechaActual=data.currentDate
 console.table(data.events);
 console.table(data.currentDate);
+
+const contenedorSearch = document.getElementById("search");
+
+function filtrarEventosChecked(eventos) {
+    checboxActivados = Array.from(document.querySelectorAll('input[type="Checkbox"]:checked')).map(checkbox => checkbox.value)
+    if (checboxActivados.length > 0) {
+        return todosLosEventos.filter(categoria => checboxActivados.includes(categoria.category))
+    } return eventos
+}
+
+function filtrarTextoIngresado(eventos) {
+   console.log(contenedorSearch.value);
+    return eventos.filter(evento=> evento.name.toUpperCase().includes(contenedorSearch.value.toUpperCase()))
+}
+contenedorCheckbox.addEventListener('change',()=>{
+
+
+});
+
+contenedorSearch.addEventListener('keyup',()=>{
+    let filtroEventosChecked= filtrarEventosChecked(todosLosEventos)
+    let filtroCoincidencias=filtrarTextoIngresado(filtroEventosChecked)
+   crearCards(filtroCoincidencias)
+});
